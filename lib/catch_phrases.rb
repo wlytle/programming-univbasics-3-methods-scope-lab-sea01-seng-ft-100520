98c7b8ce6e55d9fbf2c845420cc70201d6cf5602
Will Lytle <wlytle89@gmail.com>
2020-08-11 18:49:15 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,16 +1,19 @@
-mario_phrase = "It's-a me, Mario!";
-toadstool_phrase "Thank You Mario! But Our Princess Is In Another Castle!"
-link_phrase = "It's Dangerous To Go Alone! Take This."
+
+
+
 
 def mario
+  mario_phrase = "It's-a me, Mario!";
   puts mario_phrase
 end
 
 def toadstool
+  toadstool_phrase "Thank You Mario! But Our Princess Is In Another Castle!"
   puts toadstool_phrase
 end
 
 def link 
+  link_phrase = "It's Dangerous To Go Alone! Take This."
   puts link_phrase
 end
 
